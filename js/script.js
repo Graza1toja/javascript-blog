e2809adbb1e57da9553e17cb682c684ed118bc78
Grażyna Ciosek <gctatek@gmail.com>
2023-12-14 20:16:19 +0100

    generate list

--- js/script.js
+++ js/script.js
@@ -1,8 +1,57 @@
-'use strict';
+// 'use strict';
 /*document.getElementById('test-button').addEventListener('click', function(){
     const links = document.querySelectorAll('.titles a');
     console.log('links:', links);
-  });*/
+  });
+  */
+
+// zadeklarownae stałe / zmienne
+
+const optArticleSelector = ".post",
+  optTitleSelector = ".post-title",
+  optTitleListSelector = ".titles",
+  optArticleTagsSelector = '.post-tags .list';
+
+// zadeklarowane funkcje
+
+function generateTitleLinks () {
+  // 1. usuń zawartość listy linków w lewej kolumnie
+  // znalezienie elemnty html który zawiera listę tytułów
+  const titleList = document.querySelector(optTitleListSelector);
+  // wyczyszczenie zawartości tego elementu
+  // inner html to zawartość naszego tagu 
+  // (wszytsko co jest pomiędzy tagiem otwierającym a zamykającym)
+  // innerHTML = coś -> oznacza że to co do tej pory w innerHTML ma zostać zastąpione tym co po równa się
+  // w tym wypadku usuwane jest to co było i zastępowane pustym stringiem
+  // czyli po prostu wyczyszczone - element jest pusty
+  titleList.innerHTML = "";
+
+  // 2. dla każdego artukułu
+  // znalezienie wszystkich artykułów
+  const articles = document.querySelectorAll(optArticleSelector);
+  let html = '';
+  for(let article of articles) {
+    // odczytaj id artykułu
+    const articleId = article.getAttribute("id");
+    // znajdź element z tytułem i zapisz zawartość do stałej
+    const articleElementWithTitle = article.querySelector(optTitleSelector);
+    // wez tytuł z elementu i zapisz do stałej
+    // w tym przypadku nie zastępujemy zawartościc innerHTML tylko ją odczytujemy 
+    // (innerHTML jest po prawej stronie znaku równa się)
+    // inner HTML przypisujemy do stałej articleTitle
+    const articleTitle = articleElementWithTitle.innerHTML;
+    // stwórz HTML linku i zapisz do stałej
+    const linkHTML = '<li><a href="#'
+                    + articleId
+                    +'"><span>'
+                    + articleTitle
+                    +'</span></a></li>';
+    // wstaw html do listy linkow w lewej kolumnie
+    html = html + linkHTML;
+  }
+  titleList.innerHTML = html;
+}
+
 const titleClickHandler = function(event){
   event.preventDefault();
   const clickedElement = this;  
@@ -34,38 +83,56 @@ const titleClickHandler = function(event){
   /* add class 'active' to the correct article */
   targetArticle.classList.add('active');  
 
+  // powyzej działająca podmiana artykułu ...
+ 
+  // function generateTags(){
   
-  const links = document.querySelectorAll('.titles a');
-  
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
-  }
-  const optArticleSelector = '.post',
-  optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
-
-//funkcja generuj tytuł linku  
-function generateTitleLinks(){
-  console.log('generateTitleLinks');
-  /* remove contents of titleList - usuń zawartość listy tytułów*/
-  const titleList = generateTitleLinks.querySelector(optTitleListSelector);
-  /* for each article - dla każdego artykułu */
-  const articles =
-  for(let titleLink of titleLinks){
-
-  }
-    /* get the article id - pobierz id artykułu*/
-
-    /* find the title element - znajdz tytuł elementu  */
-
-    /* get the title from the title element - pobierz tytuł z elementu tytuł*/
-
-    /* create HTML of the link - utwórz html linku*/
-
-    /* insert link into titleList - wstaw link do listy tytułów*/
-
+  //       /* find all articles */
+  // const articles = document.querySelectorAll(optArticleSelector);
+      
+  //       /* START LOOP: for every article: */
+  //   for(let article of articles){
+  //     console.log("article");
+  //   }    
+  //       /* find tags wrapper */
+  // const tagList = article.querySelector(optArticleTagsSelector);
+    
+  //       /* make html variable with empty string */
+  //   tagList.innerHTML = "";
+        
+  //       /* get tags from data-tags attribute */
+  // const articleTags = article.getAtribut("data-tags");
+  //       console.log("articleTags:", articleTags);
+        
+  //       /* split tags into array */
+  // const articleTagsArray = articleTags.split(' ');
+  //       console.log("articleTagsArray:", articleTagsArray);
+        
+  //       /* START LOOP: for each tag */
+  //   for(let tag of articleTagsArray){
+  //       console.log("tag:",tag);
+  //   }
+  //         /* generate HTML of the link */
+  //   const linkHTML = '<li><a href="#tag'+ articleTagsArray + '">' + articleTags + '"</a></li>';
+  //       console.log("linkHTML");      
+  //   /* add generated code to html variable */
+  //   tagList.innerHTML = tagList.innerHtml + linkHTML;
+        /* END LOOP: for each tag */
+    
+        /* insert HTML of all the links into the tags wrapper */
+    
+      /* END LOOP: for every article: */
+    //}
+    
+    //generateTags();
 }
 
+// tu kończą się funkcje 
+// i zaczyna kod wywoływany bezpośrednio po uruchomieniu strony
 generateTitleLinks();
+
+const links = document.querySelectorAll('.titles a');
+
+for(let link of links){
+  link.addEventListener('click', titleClickHandler);
 }
-  
